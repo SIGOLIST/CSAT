b91ec3342fdda6fad92e59b3a2b2bafd14b14009
BowwowBread <bowwow.bread@gmail.com>
2017-11-23 17:54:25 +0900

    autocomplete search districts

--- CSAT/Frontend/components/header.vue
+++ CSAT/Frontend/components/header.vue
@@ -2,7 +2,7 @@
   <div class="header">
     <div class="container has-text-centered">
       <h1 class="title">
-        <nuxt-link to="/" class="is-black">CSAT</nuxt-link>
+        CSAT
       </h1>
     </div>
   </div>
@@ -10,10 +10,18 @@
 
 <script>
 export default {
-
+  transition: {
+    name: "header",
+    mode: "out-in"
+  }
 }
 </script>
 
 <style src="~/assets/header.scss" lang="scss" scoped>
-
+.header-leave {
+  opacity: 0;
+}
+.header-leave-active {
+  transition: all1s cubic-bezier(1,0,1,1);
+}
 </style>
--- CSAT/Frontend/components/search.vue
+++ CSAT/Frontend/components/search.vue
@@ -1,16 +1,16 @@
 <template>
-    <section>
-        <b-field>
+    <section> 
+        <b-field class="search_control" type="is-primary">
             <b-autocomplete
                 v-model="name"
                 :data="items"
                 placeholder="지역 검색"
-                field="title"
+                field="name"
                 :loading="isFetching"
                 @input="getAsyncData"
                 >
 
-                <template scope="props">
+                <!-- <template scope="props">
                     <div class="media">
                         <div class="media-left">
                             <img width="32" :src="`https://image.tmdb.org/t/p/w500/${props.option.poster_path}`">
@@ -24,36 +24,49 @@
                             </small>
                         </div>
                     </div>
-                </template>
+                </template> -->
             </b-autocomplete>
         </b-field>
+        <button class="button is-primary" @click="goEvents">검색</button>
     </section>
 </template>
 
 <script>
   import debounce from 'lodash/debounce'
+  import sigungu from '~/static/sigungu.json'
   export default {
     data() {
       return {
         items: [],
         name: '',
-        isFetching: false
+        isFetching: false,
+        districts: []
       }
     },
     created() {
+      sigungu.forEach((city) => {
+        city.gugun.forEach((district, i) => {
+          this.districts.push(district)
+        })
+      })
     },
     methods: {
+      goEvents() {
+        this.$router.push('/events')
+      },
       getAsyncData: debounce(function () {
         this.items = []
         this.isFetching = true
-        this.$axios.$get(`https://api.themoviedb.org/3/search/movie?api_key=bb6f51bef07465653c3e553d6ab161a8&query=${this.name}`)
-          .then((data) => {
-            data.results.forEach((item) => this.items.push(item))
-            this.isFetching = false
-          }, response => {
-            this.isFetching = false
+          this.items = this.districts.filter((district, i) => {
+            if(district.name.toLowerCase().indexOf(this.name.toLowerCase())>=0) {
+              return district
+            }
           })
+          this.isFetching = false
       }, 500)
     }
   }
 </script>
+<style src="~/assets/search.scss" lang="scss" scoped>
+
+</style>
