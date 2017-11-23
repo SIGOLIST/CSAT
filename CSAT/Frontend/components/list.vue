7537de1b6c66e3cf0aa22a25d348ee546cbd35f5
BowwowBread <bowwow.bread@gmail.com>
2017-11-23 17:56:11 +0900

    manage, list update

--- CSAT/Frontend/components/list.vue
+++ CSAT/Frontend/components/list.vue
@@ -34,15 +34,14 @@
                 </b-table-column>
             </template>
         </b-table>
-        <infinite-loading @infinite="infiniteHandler"></infinite-loading>
+        <infinite-loading v-if="this.data.length != 0"@infinite="infiniteHandler"></infinite-loading>
     </section>
 </template>
 
 <script>
-    import Sort from '~/components/sort.vue'
     import InfiniteLoading from 'vue-infinite-loading'
     export default {
-        components: { Sort, InfiniteLoading },
+        components: { InfiniteLoading },
         data() {
             return {
                 data: [],
@@ -60,23 +59,29 @@
             setTimeout(() => {
               this.page++
               this.loadAsyncData()
-                  $state.loaded();
-                }, 1000);
+              .then(() => {
+                $state.loaded()
+              })
+              }, 1000);
             },
             /*
              * Load async data
              */
             loadAsyncData() {
+              return new Promise((resolve, reject) => {
                 this.loading = true
                 this.$axios.$get(`https://api.themoviedb.org/3/discover/movie?api_key=bb6f51bef07465653c3e553d6ab161a8&language=en-US&include_adult=false&include_video=false&sort_by=${this.sortField}.${this.sortOrder}&page=${this.page}`)
                     .then(( data ) => {
                         data.results.forEach((item) => this.data.push(item))
                         this.loading = false
+                        resolve()
                     }, response => {
                         this.data = []
                         this.total = 0
                         this.loading = false
+                        reject()
                     })
+             })
             },
             /*
              * Handle sort event
--- CSAT/Frontend/pages/manage.vue
+++ CSAT/Frontend/pages/manage.vue
@@ -1,98 +1,14 @@
 <template>
   <div class="form">
-    <b-field label="title">
-      <b-input value="행사 제목" v-model="data.title"></b-input>
-    </b-field>
-    <b-field label="item">
-      <b-input value="상품" v-model="data.item"></b-input>
-    </b-field>
-    <b-field label="price">
-      <b-input value="가격" v-model="data.price" type="number"></b-input>
-    </b-field>
-    <b-field label="description">
-      <b-input value="설명" v-model="data.description"></b-input>
-    </b-field>
-    <b-field label="시">
-      <b-select placeholder="서울특별시" v-model="data.city">
-        <option v-for="city in cities" :value="city.name" :key="city.id">
-          {{ city.name }}
-        </option>
-      </b-select>
-    </b-field>
-    <b-field label="구">
-      <b-select placeholder="강북구" v-model="data.district">
-        <option v-for="district in districts" :value="district.name" :key="district.id">
-          {{ district.name }}
-        </option>
-      </b-select>
-    </b-field>
-    <b-field label="url">
-      <b-input value="주소" v-model="data.url"></b-input>
-    </b-field>
-    <b-field label="시작일">
-      <b-datepicker placeholder="시작일" icon="calendar-today" v-model="data.date.start" :readonly="false">
-      </b-datepicker>
-    </b-field>
-    <b-field label="종료일">
-      <b-datepicker placeholder="종료일" icon="calendar-today" v-model="data.date.end" :readonly="false">
-      </b-datepicker>
-    </b-field>
-    <button class="button is-primary" @click="addEvent">등록</button> {{data}}
-  </div>
+    <Manage/>
+  </div>  
 </template>
 
 <script>
-  import sigungu from '~/static/sigungu.json'
+  import Manage from '~/components/manage_form'
   export default {
     layout: 'manage',
-    data() {
-      return {
-        data: {
-          title: "",
-          item: "",
-          price: 0,
-          description: "",
-          location: {
-            city: "서울특별시",
-            district: "강북구",
-          },
-          url: "",
-          date: {
-            start: 0,
-            end: 0
-          }
-        },
-        cities: [],
-      }
-    },
-    created() {
-      sigungu.forEach((city, i) => {
-        this.cities.push({
-          "id": i,
-          "name": city.name
-        })
-      })
-    },
-    computed: {
-      districts() {
-        if (this.data.location.city == "") {
-          return []
-        } else {
-          const index = sigungu.findIndex(x => x.name === this.data.location.city)
-          return sigungu[index].gugun.map((district, i) => {
-            return {
-              "id": i,
-              "name": district.name
-            }
-          })
-        }
-      }
-    },
-    methods: {
-      addEvent() {
-  
-      }
-    }
+    components: { Manage } 
   }
 </script>
 
