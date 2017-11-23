d450d5349e7816d0b46d7926e630630a19792973
BowwowBread <bowwow.bread@gmail.com>
2017-11-23 17:52:40 +0900

    buefy customize

--- CSAT/Frontend/nuxt.config.js
+++ CSAT/Frontend/nuxt.config.js
@@ -1,24 +1,40 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'csat',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'csat frontend' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'csat frontend'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: "stylesheet", href: "//fonts.googleapis.com/icon?family=Material+Icons"}
+    link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      },
+      {
+        rel: "stylesheet",
+        href: "//fonts.googleapis.com/icon?family=Material+Icons"
+      }
     ]
   },
 
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: '#FFD750'
+  },
   /**
    * Plugins setup
    */
@@ -29,27 +45,39 @@ module.exports = {
   modules: [
     '@nuxtjs/axios',
   ],
+  /**
+   * Stylesheet
+   */
+  css: [
+    '~/assets/buefy.scss'
+  ],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
-    loaders: [
-  {
-    test: /\.(png|jpe?g|gif|svg)$/,
-    loader: 'url-loader',
-    query: {
-      limit: 1000, // 1초
-      name: 'img/[name].[hash:7].[ext]'
-    }
-  },
-  {
-    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-    loader: 'url-loader',
-    query: {
-      limit: 1000, // 1초
-      name: 'fonts/[name].[hash:7].[ext]'
-    }
-  }
+     postcss: {
+      plugins: {
+        'postcss-custom-properties': {
+          warnings: false
+        }
+      }
+    },
+    loaders: [{
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: 'url-loader',
+        query: {
+          limit: 1000, // 1초
+          name: 'img/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 1000, // 1초
+          name: 'fonts/[name].[hash:7].[ext]'
+        }
+      }
     ]
   },
   /**
